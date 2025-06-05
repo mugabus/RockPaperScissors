6727df10850fcda09657c2c9cd9609bc0e52dd8f
mugaabus <fatakimugaruka91@gmail.com>
2025-06-05 15:05:03 +0300

    feat: add RockPaperScissors module for smart contract deployment

---
+++ smart_contract/ignition/modules/RockPaperScissors.ts
@@ -0,0 +1,18 @@
+// This setup uses Hardhat Ignition to manage smart contract deployments.
+// Learn more about it at https://hardhat.org/ignition
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+
+
+const RockPaperScissorsModule = buildModule("RockPaperScissorsModule", (m) => {
+ 
+
+  const rockPaperScissors = m.contract("Lock", [], {
+    
+  });
+
+  return { rockPaperScissors };
+});
+
+export default RockPaperScissorsModule;
